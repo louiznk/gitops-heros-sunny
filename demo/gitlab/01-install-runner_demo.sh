8b8455b54cbfbd10f63474592d1a4ca569da2a3e
Louis Tournayre <louis.tournayre@zenika.com>
2022-04-27 22:15:17 +0200

    Refresh 3: slide & gitlab

---
+++ demo/gitlab/01-install-runner_demo.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+# Mettre à jour le token dans le fichier values.yaml (à partir de https://gitlab.com/groups/gitops-heros/-/settings/ci_cd)
+# Ou utiliser l'arg
+# Installation https://docs.gitlab.com/runner/install/kubernetes.html
+. demo-magic.sh
+set -e
+DIR=$(dirname "$0")
+pushd $DIR
+clear
+
+if [ -z "$1" ]
+then
+    echo "⚠️ - Pass the registration token as arg"
+    echo "🔗 - https://gitlab.com/groups/gitops-heros/-/settings/ci_cd"
+    flatpak run org.chromium.Chromium https://gitlab.com/groups/gitops-heros/-/settings/ci_cd 2> /dev/null &
+    exit 1
+fi
+
+
+EXIST=$(helm repo list | grep "gitlab.*https://charts.gitlab.io" | wc -l)
+if [ "x$EXIST" = "x1" ]
+then
+    ps1;pei "helm repo update gitlab"
+else
+    prompt "Add gitlab helm chart repo"    
+    pe "helm repo add gitlab https://charts.gitlab.io"
+fi
+
+
+EXIST=$(helm ls -n gitlab-runner | grep "gitlab-runner" | wc -l)
+if [ "x$EXIST" = "x1" ]
+then
+    echo "Deleting the runner already installed for update"
+    helm delete gitlab-runner -n gitlab-runner
+fi
+
+prompt "🏗️ - Deploying the GitLab Runner"
+pe "helm install --create-namespace --namespace gitlab-runner gitlab-runner -f values.yaml --set \"runnerRegistrationToken=$1\" gitlab/gitlab-runner --wait"
+
+popd
+
+echo "Go gitlab now and set variable KUBECONFIG_FILE"
+echo "🔗 - https://gitlab.com/groups/gitops-heros/-/settings/ci_cd"
+
+flatpak run org.chromium.Chromium https://gitlab.com/groups/gitops-heros/-/settings/ci_cd &> /dev/null
--- demo/gitlab/create-local-kubeconfig.sh
+++ demo/gitlab/create-local-kubeconfig.sh
@@ -1,15 +1,26 @@
 #!/bin/bash
 set -e
+txtrst=$(tput sgr0) # Text reset
+txtred=$(tput setaf 1) # Red
+txtblu=$(tput setaf 4) # Blue
+
 DIR=$(dirname "$0")
 pushd $DIR
+
+echo "${txtblu}This script create a kubeconfig file for local runner in kubeconfig.local${txtrst}"
 
 set -x
 kubectl konfig export $(kubectl config current-context)| sed "s/server:.*/server: https:\/\/kubernetes.default:443/g" > kubeconfig.local
 { set +x; } 2> /dev/null # silently disable xtrace
 
-echo "Create kubeconfig for local runner in kubeconfig.local"
+echo ""
+echo "set var ${txtred}KUBECONFIG_FILE${txtrst} with this on gitlab group"
+echo "🔗 - https://gitlab.com/groups/gitops-heros/-/settings/ci_cd"
+echo ""
 xclip -sel clip < kubeconfig.local
 
 cat kubeconfig.local
 
 popd
+
+
